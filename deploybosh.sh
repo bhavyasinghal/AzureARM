5ec0a1a7deb3b55e183ff06e9021991f4cee0d1c
bhavyasinghal <bhavya.singhal@appliedis.com>
2015-12-01 16:27:28 +0530

    Update deploybosh.sh

--- deploybosh.sh
+++ deploybosh.sh
@@ -1,3 +1,4 @@
+echo "i am starting launch script"
 for f in setup_dns.py create_cert.sh setup_devbox.py init.sh deploy_bosh.sh 98-msft-love-cf
 do
    wget $1/$f -O $f
@@ -7,6 +8,9 @@ done
 cd ../../
 python setup_devbox.py
 
+
+echo "end of launch script"
+echo "starting bosh deployment"
 #!/bin/sh
 
 export BOSH_INIT_LOG_LEVEL='Debug'
