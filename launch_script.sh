9deba4aeac6e14d65e6b3c4f59602476901159b7
bhavyasinghal <bhavya.singhal@appliedis.com>
2016-01-21 19:04:13 +0530

    Update launch_script.sh

--- launch_script.sh
+++ launch_script.sh
@@ -6,8 +6,4 @@ done
 \cp * ../../
 cd ../../
 python setup_devbox.py
-#!/bin/sh
-
-export BOSH_INIT_LOG_LEVEL='Debug'
-export BOSH_INIT_LOG_PATH='./run.log'
-bosh-init deploy bosh.yml
+sh deploy_bosh.sh
