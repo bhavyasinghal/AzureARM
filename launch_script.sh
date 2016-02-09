11ca8affc4dc4b79f0019f898cbd28c58920d66d
bhavyasinghal <bhavya.singhal@appliedis.com>
2016-02-09 19:56:06 +0000

    newcommit

--- launch_script.sh
+++ launch_script.sh
@@ -1,10 +1,10 @@
-#for f in bosh.yml setup_dns.py create_cert.sh setup_devbox.py init.sh deploy_bosh.sh 98-msft-love-cf
-#do
-#   wget $1/$f -O $f
-#done
+for f in bosh.yml setup_dns.py create_cert.sh setup_devbox.py init.sh deploy_bosh.sh 98-msft-love-cf whoamifile.sh
+do
+   wget $1/$f -O $f
+done
 
-#\cp * ../../
-#cd ../../
+\cp * ../../
+cd ../../
 #python setup_devbox.py
 #sleep 15m
 sh whoamifile.sh
